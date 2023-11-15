7d0484df46c43e3f6df0d42e3312a9bd05fbe469
shidqi29 <shidqiamirulhaq@gmail.com>
2023-11-15 21:47:36 +0700

    feat: add axios instance, custom hooks for fetching data & prettier tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl font-bold underline text-emerald-400">
+      <h1 className="p-2 text-3xl font-bold text-emerald-400 underline">
         Vite + React
       </h1>
       <div className="card">
